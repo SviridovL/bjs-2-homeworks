5f03a855312e1c8959bcde6000ed60a41128ab5e
SviridovL <sviridovl@gmail.com>
2022-09-12 15:04:38 +0300

    Final commit. all tasks done

--- 4.objects/main.js
+++ 4.objects/main.js
@@ -3,13 +3,16 @@ student1.setSubject("Algebra");
 student1.addMark(5);
 student1.addMark(4);
 student1.addMark(5);
+student1.getAverage();
 
 let student2 = new Student("Buzz", "female", 35);
 student2.setSubject("Geometry");
 student2.addMark(2);
 student2.addMark(3);
 student2.addMark(2);
-//student2.exclude('low grades')
+student2.getAverage();
+
+student2.exclude("low grades");
 
 console.log(student1);
 console.log(student2);
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -25,12 +25,26 @@ Student.prototype.addMark = function (mark) {
     this.marks.push(mark);
   }
 };
-Student.prototype.addMarks = function (...marks) {
+Student.prototype.addMarks = function (...mark) {
   if (this.marks === undefined) {
-    this.marks = [marks];
+    this.marks = mark;
   } else {
-    this.marks.push(marks);
+    this.marks.push(mark);
   }
 };
 
-//Student.prototype.addMark = function (mark) {};
+Student.prototype.getAverage = function () {
+  const average = this.marks.reduce((acc, item, idx, arr) => {
+    if (idx === this.marks.length - 1) {
+      return (acc + item) / this.marks.length;
+    } else {
+      return acc + item;
+    }
+  });
+  return (this.average = average);
+};
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
