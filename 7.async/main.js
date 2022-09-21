dc23b67b417865a38eaf80598370e82c1d13384a
SviridovL <sviridovl@gmail.com>
2022-09-21 15:46:12 +0300

    Не могу решить вызов функции

--- 7.async/main.js
+++ 7.async/main.js
@@ -1,9 +1,12 @@
 // тут вы можете вызывать функции из task.js
 let PhoneAlarm = new AlarmClock();
-PhoneAlarm.addClock("12:00", "Get up", 1);
-PhoneAlarm.addClock("12:01", "Get up 2", 2);
+PhoneAlarm.addClock("15:37", "Get up", 1);
+PhoneAlarm.addClock("15:38", "Get up 2", 2);
 //PhoneAlarm.addClock("12:00", "Get up");
-PhoneAlarm.addClock("12:00", "Get up 3", 3);
-PhoneAlarm.addClock("12:00", "Get up 4", 3);
+PhoneAlarm.addClock("15:39", "Get up 3", 3);
+PhoneAlarm.addClock("15:40", "Get up 4", 3);
 PhoneAlarm.removeClock(2);
 console.log(PhoneAlarm);
+//PhoneAlarm.getCurrentFormattedTime();
+//PhoneAlarm.start();
+PhoneAlarm.start();
--- 7.async/task.js
+++ 7.async/task.js
@@ -1,18 +1,18 @@
 class AlarmClock {
   constructor() {
     this.alarmCollection = [];
-    this.timerId = [];
+    this.timerId = null;
   }
   addClock(time, result, Id) {
     if (Id === undefined) {
       throw new Error("Id не задан");
       return;
-    } else if (this.timerId.includes(Id) === true) {
+    } else if (this.timerId === Id) {
       console.error("Такой Id уже задан");
       return;
     } else {
       let alarm = { subject: Id, value: time, result: result };
-      this.timerId.push(Id);
+      //this.timerId.push(Id);
       this.alarmCollection.push(alarm);
     }
   }
@@ -23,10 +23,36 @@ class AlarmClock {
     let endLength = this.alarmCollection.length;
     if (endLength < startLength) {
       console.log("Удаление успешно!");
+      return true;
     } else {
       console.error("Звонок не удален!");
     }
-    indx = this.timerId.indexOf(Id);
-    this.timerId.splice(indx, 1);
+  }
+
+  start() {
+    const getCurrentFormattedTime = () => {
+      let humanFormat = new Date();
+      let DateHoursMinutes = humanFormat.toString();
+      let HM = DateHoursMinutes.slice(16, 21);
+      return HM;
+    };
+    getCurrentFormattedTime();
+    //let HM = getCurrentFormattedTime();
+    for (let i = 0; i < this.alarmCollection.length; i++) {
+      console.log(this);
+      if (HM === this.alarmCollection[i].value) {
+        console.log(this.alarmCollection[i].result);
+      }
+    }
   }
 }
+
+//   if (this.timerId !== null) {
+//   }
+//   const check = () => {
+//     this.AlarmCollection.forEach((element) => {
+//       checkClock(value);
+//     });
+//   };
+//   setInterval(checkClock, 1000);
+// }
