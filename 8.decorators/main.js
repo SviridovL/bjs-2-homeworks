91bf7cbb4ca7f413cdc41cf0b30da7a7d8f4b2ca
SviridovL <sviridovl@gmail.com>
2022-09-22 21:37:23 +0300

    First task done

--- 8.decorators/main.js
+++ 8.decorators/main.js
@@ -1,15 +1,16 @@
-const addThree = (a, b, c) => (a + b + c); 
+const addThree = (a, b, c) => a + b + c;
 const upgradedAddThree = cachingDecoratorNew(addThree);
-upgradedAddThree(1, 2, 3); 
-upgradedAddThree(1, 2, 3); 
-upgradedAddThree(2, 2, 3); 
-upgradedAddThree(3, 2, 3); 
-upgradedAddThree(4, 2, 3); 
-upgradedAddThree(5, 2, 3); 
-upgradedAddThree(6, 2, 3); 
+upgradedAddThree(1, 2, 3);
+upgradedAddThree(1, 2, 3);
+upgradedAddThree(2, 2, 3);
+upgradedAddThree(3, 2, 3);
+upgradedAddThree(4, 2, 3);
+upgradedAddThree(5, 2, 3);
+upgradedAddThree(6, 2, 3);
 upgradedAddThree(1, 2, 3);
 
-const sendSignal = (signalOrder, delay) => console.log("Сигнал отправлен", signalOrder, delay);
+const sendSignal = (signalOrder, delay) =>
+  console.log("Сигнал отправлен", signalOrder, delay);
 const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
 setTimeout(() => upgradedSendSignal(1, 0)); // Сигнал отправлен + будет запланирован асинхронный запуск, который будет проигнорирован так как следующий сигнал отменит предыдущий (300 - 0 < 2000)
 setTimeout(() => upgradedSendSignal(2, 300), 300); // проигнорировано так как следующий сигнал отменит предыдущий (900 - 300 < 2000)
@@ -21,4 +22,4 @@ setTimeout(() => upgradedSendSignal(7, 4500), 4500); // Сигнал будет 
 setTimeout(() => {
   console.log(upgradedSendSignal.count); // было выполнено 3 отправки сигнала
   console.log(upgradedSendSignal.allCount); // было выполнено 6 вызовов декорированной функции
-}, 7000)
+}, 7000);
--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,7 +1,27 @@
 function cachingDecoratorNew(func) {
   // Ваш код
-}
+  let cache = [];
+  function wrapper(...args) {
+    const hash = args.join`,`;
 
+    const objinCache = cache.find((item) => obj.subject === hash);
+    if (objinCache) {
+      console.log("Из кэша  " + objinCache.value);
+      return "Из кэша  " + objinCache.value;
+    }
+    if (!objinCache) {
+      let result = func(...args);
+      obj = { subject: hash, value: result };
+      cache.push(obj);
+      if (cache.length > 5) {
+        cache.shift;
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем  " + result;
+    }
+  }
+  return wrapper;
+}
 
 function debounceDecoratorNew(func) {
   // Ваш код
